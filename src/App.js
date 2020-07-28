0c2a7608ed85f193c353409e1d723c0b714c98e5
Kyle Wright <kylejamwright@gmail.com>
2020-07-27 23:31:00 -0600

    started skeleton to roll old dice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Dice from './dice';
-import RandomNum from './randomNum'
+// import RandomNum from './randomNum'
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentMax: null,
-      diceList: []
+      diceList: [],
+      randomNum: 0
     };
   }
   
@@ -22,14 +23,30 @@ export default class App extends React.Component {
     }, () => console.log(this.state.diceList));
   }
 
+  calcRandomNum() {
+    if (this.state.diceList.length > 0) {
+      return Math.ceil(Math.random() * this.state.diceList[this.state.diceList.length - 1]);
+    } else {
+      return 0;
+    }
+  } 
+
+  handleOldDiceClick(event) {
+    // event.preventDefault();
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Welcome to the Dice Game</h1>
-        <RandomNum value={this.state.currentMax} />
         <Dice onClick={(dice) => this.handleAddDiceClick(dice)}/>
+        <h3>Your random number: {this.calcRandomNum()}</h3>
+        <h4>Your dice: </h4>
         <ul>
-          {this.state.diceList.map((dice, index) => <li key={index}>{dice}</li>)}
+          {this.state.diceList.map((dice, index) => <button
+            key={index}
+
+          >{dice}</button>)}
         </ul>
       </div>
     );
